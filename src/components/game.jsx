eab456132cb601b640daf93de5b1b6b0d1924717
qtpeters <quinn.t.peters@gmail.com>
2019-01-05 09:39:51 -0500

    Added gsap

--- src/components/game.jsx
+++ src/components/game.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 
 class Game extends React.Component {
   render() {
-    return ( <h2>Game Starts here</h2> );
+    return ( 
+      <h2>Game Starts here</h2> 
+      
+    );
   }
 }
 
